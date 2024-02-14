582542624f82f5e28306515e9273a0ae68e44c8c
suplis <israel.surribas.planas@gmail.com>
2024-02-14 12:32:44 +0100

    Exemplos contextos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Component1 from './componente/Component1';
+import ComponenteTheme from './componente/ComponenteTheme';
+import DousContextos from './componente/DousContextos';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Component1 />
+      <ComponenteTheme />
+      <DousContextos />
     </div>
   );
 }
